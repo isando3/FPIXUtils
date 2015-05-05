8154cbd1ed09873e52f048a0dcd00c6dbd919aa8
jstupak <jstupak@fnal.gov>
2015-05-04 22:46:19 -0500

    files to help shifters evaluate test results

---
+++ Comparison.py
@@ -0,0 +1,118 @@
+#! /usr/bin/env python
+
+"""
+Author: John Stupak (jstupak@fnal.gov)
+Date: 5-2-15
+Usage: ./checkPretest.py <input dir>
+"""
+
+import ROOT
+ROOT.gErrorIgnoreLevel = ROOT.kWarning
+from ROOT import *
+gStyle.SetOptStat(0)
+import math
+
+################################################################
+################################################################
+################################################################
+
+goodModules=[]
+
+def click(moduleNo, goodModules, testPad):
+
+    if gPad.GetEvent()==11:
+        if moduleNo in goodModules:
+            goodModules.remove(moduleNo)
+
+            testPad.cd(moduleNo+1)
+            gPad.SetFillColor(kRed)
+            gPad.Modified()
+            gPad.Update()
+        else:
+            goodModules+=[moduleNo]
+            goodModules.sort()
+
+            testPad.cd(moduleNo+1)
+            gPad.SetFillColor(kGreen)
+            gPad.Modified()
+            gPad.Update()
+
+    return None
+
+################################################################
+################################################################
+################################################################
+
+class Comparison:
+    
+    def __init__(self, hName, refName, referenceFile, outputDir, info='All plots should resemble the reference plot'):
+        self.hName=hName
+        self.refFile=TFile(referenceFile)
+        self.outputDir=outputDir
+        self.info=info
+
+    #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
+    def do(self, testFiles):
+        testFiles=[TFile(f) for f in testFiles]
+
+        moduleNames=['_'.join(f.GetName().split('/')[-3].split('_')[:-4]) for f in testFiles]
+        nModules=len(moduleNames)
+        
+        global goodModules
+        goodModules=[]
+        
+        c=TCanvas('c','',600,800)
+
+        refPad=TPad('refPad','Reference Plot',.666,0.25,1,0.75)
+        refPad.Draw()
+        refPad.cd()
+
+        ref=self.refFile.Get(self.hName).Clone('REF__'+self.hName.split('/')[-1])
+        is2D=(type(ref)==type(TH2D()))
+        ref.Draw('COLZ'*is2D)
+
+        c.cd()
+        testPad=TPad('testPad','',0,0,.666,1)
+        testPad.Divide(int(math.ceil(nModules/2.)),2)
+        testPad.Draw()
+
+        histograms=[]
+        for i in range(nModules):
+            testPad.cd(i+1)
+
+            h=testFiles[i].Get(self.hName).Clone(moduleNames[i]+'__'+self.hName.split('/')[-1])
+            h.SetTitle(moduleNames[i])
+            h.Draw('COLZ'*is2D)
+            histograms.append(h)
+
+            gPad.Modified()
+            gPad.Update()
+            gPad.SetFillColor(kRed)
+            gPad.Modified()
+            gPad.Update()
+
+            gPad.AddExec('exec','TPython::Exec( "click('+str(i)+', goodModules, testPad)" )')
+
+        c.Modified()
+        c.Update()
+
+        while True:
+            input=raw_input('\n'+self.info+'\n\n'+'Press enter to submit results\n'+'Enter "-1" to go back a test\n\n')
+            if input=='-1': 
+                refPad.Close()
+                testPad.Close()
+                c.Close()
+                return -1
+
+            if input=='':
+                badModules=[x for x in range(nModules) if x not in goodModules]
+                for i in badModules:
+                    testPad.GetPad(i+1).SaveAs(self.outputDir+'/'+histograms[i].GetName()+'.pdf')
+                badModuleNames=[moduleNames[i] for i in badModules]
+
+                refPad.Close()
+                testPad.Close()
+                c.Close()
+
+                return badModuleNames
---
+++ checkPretest.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+
+"""
+Author: John Stupak (jstupak@fnal.gov)
+Date: 5-2-15
+Usage: ./checkPretest.py <input dir>
+"""
+
+from Comparison import *
+
+from sys import argv
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if len(argv)>1:
+    testFiles=argv[1:]
+else:
+    testFiles=['/Users/jstupak/CMS/pixel/ShareTestResults/M_LL_922_ElComandanteTest_2015-04-28_10h33m_1430235234/001_Pretest_p17/commander_Pretest.root',
+               '/Users/jstupak/CMS/pixel/ShareTestResults/M_TT_915_ElComandanteTest_2015-04-28_10h33m_1430235234/001_Pretest_p17/commander_Pretest.root',
+               '/Users/jstupak/CMS/pixel/ShareTestResults/P-A-03-42_ElComandanteTest_2015-04-28_10h33m_1430235234/001_Pretest_p17/commander_Pretest.root',
+               ]
+
+referenceFile='/Users/jstupak/CMS/pixel/ShareTestResults/M_FR_902_ElComandanteTest_2015-04-16_15h24m_1429215874/001_Pretest_p17/commander_Pretest.root'
+
+outputDir='tmp'
+
+theComparisons=[Comparison('Pretest/programROC_V0','Pretest/programROC_V0',referenceFile,outputDir,'All y values should be greater than 0'),
+                Comparison('Pretest/Iana_V0','Pretest/Iana_V0',referenceFile,outputDir,'All y values should be approximately 24')]
+theComparisons+=[Comparison('Pretest/pretestVthrCompCalDel_c12_r22_C'+str(i)+'_V0','Pretest/pretestVthrCompCalDel_c12_r22_C0_V0',referenceFile,outputDir) for i in range(16)]
+
+################################################################
+################################################################
+################################################################
+
+if __name__=='__main__':
+
+    results=[]
+    i=0
+    badModules=[]
+    while i<len(theComparisons):
+        result=theComparisons[i].do(testFiles)
+        
+        #go back one test
+        if result==-1: i=max(i-1,0)
+
+        if type(result)==type([]):
+            try: results[i]=result
+            except: results.append(result)
+            i+=1
+
+    badModules=set([])
+    for result in results:
+        badModules=badModules|set(result)
+    badModules=list(badModules)
+    if len(badModules)>0:
+        print 'Replace the following module(s) and repeat pre-test:'
+        for m in badModules:
+            print '    - '+str(m)
+    else:
+        print 'Rock on'
+        
