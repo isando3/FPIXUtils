3082fc916687debec9d2db5a3533f539f1706acd
jstupak <jstupak@fnal.gov>
2015-05-14 14:16:06 -0500

    bug fix

--- uploadTest.py
+++ uploadTest.py
@@ -2,13 +2,15 @@
 
 from config import *
 from lessWeb import *
+from sys import argv
 
-if len(argv)>1:
-    doCold=argv[1]
+if len(argv)>2:
+    testName=argv[1]
+    doCold=argv[2]
 else:
     raise Exception("You must specify if test was performed cold")
 
 for module in moduleNames:
-    input=sorted(glob('/home/fnalpix?/ShareTestResults/'+module+'_ElComandanteTest_*/*_'+testName.capitalize()+'_*'))[-1]
+    input=sorted(glob('/home/fnalpix?/ShareTestResults/'+module+'_ElComandanteTest_*/*_'+testName+'_*'))[-1]
     makeXML(input)
     
