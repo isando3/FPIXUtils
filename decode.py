1940cd54d0e763f1a81367417ba9d0ddb06e502d
malhusse <malhusse@fnal.gov>
2015-06-19 15:06:52 -0500

    modified version

--- decode.py
+++ decode.py
@@ -1,31 +1,67 @@
 import sys
 
-header='011111111100'
+bits = ''
 
-
-bits = '' # ends when this string is seen
 while True:
     i=raw_input()
     if i.strip()=='': break
     bits+=i.split('=')[1].strip()
 
-n=bits.find(header)
-if n==-1:
-    print
-    print 'No header found!!!'
-    print
-    print bits
-    exit()
+bits=bits[bits.find('0'):]
+
+# Check Header
+print bits[:12],
+if bits[:12]=='011111111100': 
+    print '   (Header)'
 else:
-    bits=bits[bits.find(header):]
-    print
-    print bits[:12],
-    if bits[:12]=='011111111100': print '   (Header)'
-    print bits[12:20],
-    if bits[12:20]=='00000001': print '       (Event No)'
-    print bits[20:28],'       (Data ID)'
-    print bits[28:40],
-    if bits[28:40]=='011111111110': print '   (Trailer)'
-    elif bits[28:40]=='000001111110': print '   (Trailer - bits 2-5 flipped)'
-    print bits[40:48],'       (Event Data)'
-    print bits[48:56],'       (Stack Count)'
+    print '   (Header - bits',
+    for i in range(1,9):
+        if bits[i]=='0':
+            print i, 
+    for j in range(10,12):
+        if bits[j]=='1':
+            print j,
+    print 'flipped)'
+
+# Check Event No
+print bits[12:20],
+if bits[12:20]=='00000001': print '       (Event No)'
+else:
+    print '       (Event No is not one!)'
+
+# Check Data ID
+print bits[20:28],'       (Data ID)'
+# NEED TO TEST A TBM TO DETERMINE GOOD VALUES
+
+# Check Trailer
+print bits[28:40],
+if bits[28:40]=='011111111110': print '   (Trailer)'
+else:
+    print '   (Trailer - bits',
+    for i in range(29,36):
+        if bits[i]=='0':
+            print i-27,
+    if bits[39]=='1':
+            print '39',
+    print 'flipped)'
+
+# Check Event Info
+print bits[40:50],'     (Event Info)'
+if bits[40]=='1': print 'No Token Pass!'
+if bits[41]=='0': print 'TBM Reset not sent'
+if bits[42]=='0': print 'ROC Reset not sent'
+if bits[43]=='1': print 'Sync Error'
+if bits[44]=='1': print 'Sync Trigger'
+if bits[45]=='1': print 'Clear Trigger Cntr'
+if bits[46]=='0': print 'Cal Trigger'
+if bits[47]=='1': print 'Stack Full'
+if bits[48]=='1': print 'Auto Reset Sent'
+if bits[49]=='1': print 'Pkam Reser Sent'
+
+
+
+# Check Stack Count
+print bits[50:56],
+if bits[50:56]=='000001':print'         (Stack Count)'
+else:
+    print '       (Stack Count is not one!)'
