457af03eff160b13c937bb7e4be66cdb78a3509a
Jamie Antonelli <lantone@gmail.com>
2015-12-01 15:21:00 -0600

    made plot-finding recursive and added a '-m pos' option for parsing output files from the pixel online software

--- addModuleSummaryPlots.py
+++ addModuleSummaryPlots.py
@@ -5,6 +5,8 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-i", "--file", dest="inputFileName",
                   help="path to input file")
+parser.add_option("-m", "--mode", dest="mode", default = "pxar",
+                  help="supported modes are 'pxar' (which is default) and 'pos' (pixel online software)")
 (arguments, args) = parser.parse_args()
 
 if not arguments.inputFileName:
@@ -16,9 +18,9 @@ from moduleSummaryPlottingTools import *
 gROOT.SetBatch()
 
 # add together appropriate 1D plots per ROC into a single 1D summary
-dictionary1D = produce1DHistogramDictionary(arguments.inputFileName)
-add1DSummaryPlots(arguments.inputFileName, dictionary1D)
+dictionary1D = produce1DHistogramDictionary(arguments.inputFileName, arguments.mode)
+add1DSummaryPlots(arguments.inputFileName, dictionary1D, arguments.mode)
 
 # arrange 2D ROC plots into a single 2D summary plot
-dictionary2D = produce2DHistogramDictionary(arguments.inputFileName)
-add2DSummaryPlots(arguments.inputFileName, dictionary2D)
+dictionary2D = produce2DHistogramDictionary(arguments.inputFileName, arguments.mode)
+add2DSummaryPlots(arguments.inputFileName, dictionary2D, arguments.mode)
